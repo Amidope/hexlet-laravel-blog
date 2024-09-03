12a361c8801d840c11e1cb37e66d1f9dd6931858
Amidope <noreply>
2024-09-03 18:09:55 +0500

    add

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class PageController extends Controller
 {
-    public function about()
+    public function about(): View
     {
         return view('page.about');
     }
